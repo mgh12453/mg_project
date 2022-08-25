995e34547ea775cbcd475dcf7a06024ba6965d1f
mgh12453 <mgh12453@gmail.com>
2022-08-26 00:35:34 +0430

    Add hello application to project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first_page/', include('first_page.urls')),
+    path('hello/', include('hello.urls')),
 ]
