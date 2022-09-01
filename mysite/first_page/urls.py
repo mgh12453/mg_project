46f197e66b337354bd32b53da853807eef519a38
mgh12453 <mgh12453@gmail.com>
2022-09-01 21:50:51 +0430

    Add session using to first_page

--- mysite/first_page/urls.py
+++ mysite/first_page/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', views.show_page, name='show_page'),
     path('<int:page>', views.show_page, name='show_page_num'),
     path('info/<int:id>', views.info, name='info'),
-    path('next<int:page>', views.next_page, name='next_page'),
-    path('previous<int:page>', views.previous_page, name='previous_page'),
 ]
--- mysite/first_page/views.py
+++ mysite/first_page/views.py
@@ -2,29 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator
 from .models import Task 
+num_of_page = 0
 
-def show_page(request, page=1):
-    try:
-        p = Paginator(Task.objects.all(), 5)
-        context = {'page': page, 'task': p.page(page).object_list}
-        return render(request, 'first_page/index.html', context)
-    except:
-        return None
+def show_page(request, page=None):
+    p = Paginator(Task.objects.all(), 5)
+
+    if page != None:
+        if p.num_pages < page:
+            page = 1
+        if page < 1:
+            page = p.num_pages 
+        request.session['page'] = page
+
+    page = request.session.get('page', 1)
+    context = {'page': page, 'num_page': p.num_pages, 'task': p.page(page).object_list}
+    return render(request, 'first_page/index.html', context)
 
 def info(request, id):
     task = Task.objects.get(pk=id)
     context = {'task': task}
     return render(request, 'first_page/info.html', context)
     
-
-def next_page(request, page):
-    show = show_page(request, page+1)
-    if show == None:
-        return show_page(request, 1)
-    return show
-
-def previous_page(request, page):
-    show = show_page(request, page-1)
-    if show == None:
-        return show_page(request, int(Task.objects.count()/5))
-    return show
